993095a2835befd8d68eb6e826ffef93ec484e59
Sean Haugh <sean@lfo.team>
2019-01-21 08:57:15 -0600

    Set dmenu2 size dynamically

--- dmenu2-flags.sh
+++ dmenu2-flags.sh
@@ -1,14 +1,21 @@
-# shellcheck source=/home/sean/.cache/wal/colors.sh
+# shellcheck source=/home/swim/.cache/wal/colors.sh
 source ~/.cache/wal/colors.sh \
-	|| notify-send dmenu-run-wrapper "Couldn't find a wal colorscheme."
+	|| notify-send dmenu_run "Couldn't find a wal colorscheme."
+
+geom=($(</sys/class/graphics/fb0/virtual_size tr "," " "))
+width=$((3 * geom[0] / 5))
+height=40
+xpos=$(((geom[0] - width) / 2))
+ypos=$(((5 * geom[1] - height) / 12))
 
 export extraFlagsArray+=(
 	"-q"
-	"-fn" "sans-serif:size=16"
+	"-fn" "sans-serif:size=12"
 	"-nb" "${background:-#000000}"
 	"-sb" "${color3:-#ffffff}"
 	"-nf" "${foreground:-#ffffff}"
 	"-sf" "${background:-#000000}"
-	"-x" "480" "-y" "540"
-	"-w" "960" "-h" "50"
+	"-x" "$xpos" "-y" "$ypos"
+	"-w" "$width" "-h" "$height"
+	"-dim" "0.8"
 )
