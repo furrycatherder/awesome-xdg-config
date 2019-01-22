8df460b4df1d9cc0106a66519958310acf338442
Sean Haugh <sean@lfo.team>
2019-01-22 11:53:34 -0600

    Add workarounds for pywal issue

--- default.nix
+++ default.nix
@@ -13,6 +13,23 @@
       hardeningDisable = [ "all" ];
     });
 
+    pywal = previous.pywal.overrideAttrs (super: {
+      src = previous.fetchFromGitHub {
+        owner = "furrycatherder";
+        repo = "pywal";
+        rev = "4aa28b1bdb641929768748e09ea82e424c168b07";
+        sha256 = "01khh1sv4cgws13ykyanshkyjhjx5m6mj6q6rw01d6yqw9nbcpjk";
+      };
+    });
+
+    wpgtk = previous.wpgtk.overrideAttrs (super: {
+      propagatedBuildInputs = with final.python3Packages; [
+        pygobject3
+        pillow
+        final.pywal
+      ];
+    });
+
     dmenu2 = previous.dmenu2.overrideAttrs (super: {
       buildInputs = super.buildInputs ++ [ makeWrapper ];
       postFixup = ''
