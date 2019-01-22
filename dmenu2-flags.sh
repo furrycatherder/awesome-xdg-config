290834c5618b63e06052ed277a37b2df6259d5f5
Sean Haugh <sean@lfo.team>
2019-01-22 12:01:05 -0600

    Fix dmenu colors and improve error handling

--- dmenu2-flags.sh
+++ dmenu2-flags.sh
@@ -1,6 +1,8 @@
 # shellcheck source=/home/swim/.cache/wal/colors.sh
-source ~/.cache/wal/colors.sh \
-	|| notify-send dmenu_run "Couldn't find a wal colorscheme."
+source ~/.config/wpg/formats/colors.sh \
+	|| source ~/.cache/wal/colors.sh \
+	|| nix run nixpkgs.libnotify -c \
+	notify-send dmenu_run "Couldn't find a wal colorscheme."
 
 geom=($(</sys/class/graphics/fb0/virtual_size tr "," " "))
 width=$((3 * geom[0] / 5))
@@ -10,11 +12,11 @@ ypos=$(((5 * geom[1] - height) / 12))
 
 export extraFlagsArray+=(
 	"-q"
-	"-fn" "sans-serif:size=12"
-	"-nb" "${background:-#000000}"
-	"-sb" "${color3:-#ffffff}"
-	"-nf" "${foreground:-#ffffff}"
-	"-sf" "${background:-#000000}"
+	"-fn" "sans-serif"
+	"-nb" "${background:-#ffffff}"
+	"-sb" "${color2:-#ffffff}"
+	"-nf" "${foreground:-#000000}"
+	"-sf" "${foreground:-#000000}"
 	"-x" "$xpos" "-y" "$ypos"
 	"-w" "$width" "-h" "$height"
 	"-dim" "0.8"
