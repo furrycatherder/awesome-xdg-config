7223140bba19935f8c91d51fda6e09096e55d9d1
Sean Haugh <sean@lfo.team>
2019-01-22 13:07:56 -0600

    Fix wallpaper setting

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -35,7 +35,7 @@ awful.layout.layouts = {
     awful.layout.suit.max.fullscreen,
 }
 
-awful.spawn("~/.config/wpg/wp_init.sh")
+awful.spawn(os.getenv("HOME") .. "/.config/wpg/wp_init.sh")
 awful.spawn("compton -b --vsync opengl-mswc --backend glx \
 	--blur-method kawase --blur-background --blur-strength 10")
 
