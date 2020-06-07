e831f9ff607e05b507b8eb9ccbe8366b90be134d
Sean Haugh <shaugh@sunpower.com>
2020-06-07 15:02:50 -0500

    Make this an overlay

--- default.nix
+++ default.nix
@@ -1,48 +1,48 @@
-{
-  overlays = final: previous: with final; {
-    arch-xdg-menu = callPackage ./arch-xdg-menu.nix {};
+final: previous:
+with final; {
+  arch-xdg-menu = callPackage ./arch-xdg-menu.nix { };
 
-    compton-kawase = previous.compton.overrideAttrs (super: {
-      src = previous.fetchFromGitHub {
-        owner = "tryone144";
-        repo = "compton";
-        rev = "241bbc50285e58cbc6a25d45066689eeea913880";
-        sha256 = "148s7rkgh5aafzqdvag12fz9nm3fxw2kqwa8vimgq5af0c6ndqh2";
-      };
+  compton-kawase = previous.compton.overrideAttrs (super: {
+    src = previous.fetchFromGitHub {
+      owner = "tryone144";
+      repo = "compton";
+      rev = "241bbc50285e58cbc6a25d45066689eeea913880";
+      sha256 = "148s7rkgh5aafzqdvag12fz9nm3fxw2kqwa8vimgq5af0c6ndqh2";
+    };
 
-      hardeningDisable = [ "all" ];
-    });
+    hardeningDisable = [ "all" ];
+  });
 
-    pywal = previous.pywal.overrideAttrs (super: {
-      src = previous.fetchFromGitHub {
-        owner = "furrycatherder";
-        repo = "pywal";
-        rev = "9a764c29fd8f493555f73cedb696a89191f702cb";
-        sha256 = "0xvrhr1asymhjkm7wgv6jar100apyyii9m4k17i0cv4pa67pjqdc";
-      };
-    });
+  pywal = previous.pywal.overrideAttrs (super: {
+    src = previous.fetchFromGitHub {
+      owner = "furrycatherder";
+      repo = "pywal";
+      rev = "9a764c29fd8f493555f73cedb696a89191f702cb";
+      sha256 = "0xvrhr1asymhjkm7wgv6jar100apyyii9m4k17i0cv4pa67pjqdc";
+    };
+  });
 
-    wpgtk = previous.wpgtk.overrideAttrs (super: {
-      propagatedBuildInputs = with final.python3Packages; [
-        pygobject3
-        pillow
-        final.pywal
-      ];
-    });
+  wpgtk = previous.wpgtk.overrideAttrs (super: {
+    propagatedBuildInputs = with final.python3Packages; [
+      pygobject3
+      pillow
+      final.pywal
+    ];
+  });
 
-    dmenu = previous.dmenu.overrideAttrs (super: {
-      buildInputs = super.buildInputs ++ [ makeWrapper ];
-      patches = let
-        fetchPatch = path: builtins.fetchurl "https://tools.suckless.org/dmenu/patches/${path}";
-      in super.patches ++ [
-        (fetchPatch "line-height/dmenu-lineheight-4.9.diff")
-        (fetchPatch "xyw/dmenu-xyw-4.7.diff")
-        ./dmenu-dim-20191228-cbef4ab.diff
-        ./dmenu-tabright-20191228-acc1cee.diff
-      ];
-      postFixup = ''
-        wrapProgram $out/bin/dmenu_run --run "source ${./dmenu-flags.sh}"
-      '';
-    });
-  };
+  dmenu = previous.dmenu.overrideAttrs (super: {
+    buildInputs = super.buildInputs ++ [ makeWrapper ];
+    patches = let
+      fetchPatch = path:
+        builtins.fetchurl "https://tools.suckless.org/dmenu/patches/${path}";
+    in super.patches ++ [
+      (fetchPatch "line-height/dmenu-lineheight-4.9.diff")
+      (fetchPatch "xyw/dmenu-xyw-4.7.diff")
+      ./dmenu-dim-20191228-cbef4ab.diff
+      ./dmenu-tabright-20191228-acc1cee.diff
+    ];
+    postFixup = ''
+      wrapProgram $out/bin/dmenu_run --run "source ${./dmenu-flags.sh}"
+    '';
+  });
 }
